0e2fc0acbb96c10c0f0dffa8c896ac5f0f050d80
liemhome <bongden100w@gmail.com>
2026-01-12 23:31:41 +0900

    remove album

--- proposalScript.js
+++ proposalScript.js
@@ -13,10 +13,9 @@ let musicStarted = false;
 let currentPage = 0;
 let startY = 0;
 let currentIndex = 0;
-let slideInterval = null;
+
 const pages = document.querySelectorAll('.page');
 const dots = document.querySelectorAll('.dot');
-const photos = ['pics/aodaido.jpg', 'pics/aodaihong1.jpg', 'pics/aodaixanhnhat.jpg', 'pics/dim1.jpg', 'pics/longbien.jpg'];
 
 const firstMessageDate = new Date('2025-07-13 21:43');
 
@@ -64,8 +63,7 @@ function handleTouchEnd(evt) {
 }
 
 function showPage(pageIndex) {
-    // Always stop slideshow when changing pages
-    stopSlideshow();
+
     // Hide current page
     pages[currentPage].classList.remove('active');
     dots[currentPage].classList.remove('active');
@@ -74,46 +72,8 @@ function showPage(pageIndex) {
     dots[pageIndex].classList.add('active');
     currentPage = pageIndex;
 
-    if (pageIndex === 2) {
-        startSlideshow();
-    }
 }
 
-//album on page 3
-function stopSlideshow() {
-    if (slideInterval) {
-        clearInterval(slideInterval);
-        slideInterval = null;
-    }
-}
-
-function startSlideshow() {
-    stopSlideshow();
-    slideInterval = setInterval(() => {
-        currentIndex = (currentIndex + 1) % photos.length;
-        showPhoto(currentIndex);
-    }, 2000);
-}
-
-function showPhoto(index) {
-    const mainPhoto = document.getElementById('currentPhoto');
-
-    // Fade out
-    mainPhoto.style.opacity = '0';
-
-    setTimeout(() => {
-        currentIndex = index;
-        mainPhoto.src = photos[index];
-
-        // Fade in
-        mainPhoto.style.opacity = '1';
-
-        // Update active thumbnail
-        document.querySelectorAll('.thumb').forEach((thumb, i) => {
-            thumb.classList.toggle('active', i === index);
-        });
-    }, 250); 
-}
 
 // Keyboard support for testing
 document.addEventListener('keydown', (evt) => {
@@ -123,7 +83,6 @@ document.addEventListener('keydown', (evt) => {
         showPage(currentPage - 1);
     }
 });
-
 
 
 function showQuestion() {
